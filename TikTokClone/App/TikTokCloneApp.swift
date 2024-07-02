e2ab9941b08bd381918a1eb528027f18423a4aa8
Treybrowder <tbrowder7@gmail.com>
2024-07-02 14:45:19 -0400

    First commit

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Trey Browder on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
