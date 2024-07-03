61bc27906bdc0e35be46fb3a1a490b4551bdc050
Treybrowder <tbrowder7@gmail.com>
2024-07-03 14:14:07 -0400

    Added explore page user cell

---
+++ TikTokClone/Core/Search/UserCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCell.swift
+//  TikTokClone
+//
+//  Created by Trey Browder on 7/3/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCell()
+}
